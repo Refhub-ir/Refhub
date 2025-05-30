a4b7d16f06d6fb4f59c86a8a20e7a5a34dc80fce
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-30 21:18:18 +0330

    Add CommentBookViewComponent for handling book comments
    Introduces a new `CommentBookViewComponent` class in the
    `Refhub_Ir.Components.Handlers.Home` namespace. This class
    inherits from `ViewComponent` and includes a constructor
    that accepts an `IBookService` parameter. The `InvokeAsync`
    method is implemented to return a view based on a default
    view path obtained from `GetDefaultViewPath()`. Necessary
    namespaces for `Microsoft.AspNetCore.Mvc` and
    `Refhub_Ir.Service.Interface` are also imported.

---
+++ Refhub_Ir/Components/Handlers/Home/CommentBookViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refhub_Ir.Service.Interface;
+
+namespace Refhub_Ir.Components.Handlers.Home
+{
+    public class CommentBookViewComponent
+        (IBookService bookService)
+        : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync( CancellationToken ct)
+        {
+            string viewPath = this.GetDefaultViewPath();
+            return View(viewPath);
+        }
+
+    }
+}
