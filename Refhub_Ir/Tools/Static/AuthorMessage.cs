a55732d889d1407294fcf358b54e4c41247302d0
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-31 20:33:06 +0330

    Add static message author

---
+++ Refhub_Ir/Tools/Static/AuthorMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Refhub_Ir.Tools.Static
+{
+    public static class AuthorMessage
+    {
+
+        public const string Error_NotDefinde = "نامشخص";
+        public const string Error_SlugExists = "اسلاگ قبلاً استفاده شده است";
+        public const string Error_AuthorNotfound = "نویسنده پیدا نشد";
+      
+    }
+}
