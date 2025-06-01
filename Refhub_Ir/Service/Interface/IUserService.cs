c10312c2102c69de2aa8e6b66587e944a14f1205
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-06-01 14:38:06 -0800

    Sort code

--- Refhub_Ir/Service/Interface/IUserService.cs
+++ Refhub_Ir/Service/Interface/IUserService.cs
@@ -1,18 +1,15 @@
 ﻿using ErrorOr;
-using Refhub_Ir.Models.Categories;
 using Refhub_Ir.Models.Users;
 
 namespace Refhub_Ir.Service.Interface
 {
     public interface IUserService
     {
-    
-        Task Logout( CancellationToken ct);
-        Task <IEnumerable<UserListAdminVM>> GetListUserAdminPanelAsync(string? name, CancellationToken ct);
-        Task <ErrorOr<UserListAdminVM>> AddToRoleForUserInAdminPanelAsync(string id, CancellationToken ct);
+        Task Logout(CancellationToken ct);
+        Task<IEnumerable<UserListAdminVM>> GetListUserAdminPanelAsync(string? name, CancellationToken ct);
+        Task<ErrorOr<UserListAdminVM>> AddToRoleForUserInAdminPanelAsync(string id, CancellationToken ct);
         Task<ErrorOr<LoginVM>> Login(LoginVM model, CancellationToken ct);
         Task<ErrorOr<RegisterVM>> Register(RegisterVM model, CancellationToken ct);
-
     }
 }
 
