5f27082eb8b3f1788967f4d81881069ad02ff1e6
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-04 13:04:07 +0330

    add bookItemVM

---
+++ Refhub_Ir/Models/Books/BookItemVM.cs
@@ -0,0 +1,13 @@
+﻿namespace Refhub_Ir.Models.Books;
+
+public class BookItemVM
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string CategoryName { get; set; }
+    public string Authores { get; set; }
+   
+    public string ImagePath { get; set; }
+    
+}
