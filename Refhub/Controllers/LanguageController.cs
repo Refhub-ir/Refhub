78fec55bb52b66378b269507d2014105134d565f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 23:24:03 +0330

    add multiLanguage controller add route fa/en and testmessage

---
+++ Refhub/Controllers/LanguageController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Refhub.Service.Implement;
+using Refhub.Service.Interface;
+
+namespace Refhub.Controllers;
+
+public class LanguageController(IMessageService messageService) : Controller
+{
+
+
+    [Route("/fa")]
+    public IActionResult persian()
+    {
+        Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("fa-IR")),
+            new CookieOptions() { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+        return RedirectToAction("Index", "Home");
+    }
+    [Route("/en")]
+    public IActionResult english()
+    {
+        Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-US")),
+            new CookieOptions() { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+        return RedirectToAction("Index","Home");
+    }
+
+    [Route("/TestMessage")]
+    public IActionResult TestMessage()
+    {
+
+        return Content(messageService.Get("Title"));
+    }
+
+}
