ec5e6d59d0ab0f711c9b2011e8f862f0a15e6776
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-08 23:27:06 +0330

    add on ViewModel

--- Refhub/Models/Authors/authorDTO.cs
+++ Refhub/Models/Authors/authorDTO.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Refhub.Resources;
 
 namespace Refhub_Ir.Areas.Admin.DTOs;
 
 public class AuthorDTO
 {
-    [Required(ErrorMessage = "نام کامل الزامی است")]
+    [Required(ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.FullNameRequired))]
     public string FullName { get; set; }
 
-    [Required(ErrorMessage = "اسلاگ الزامی است")]
+    [Required(ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.SlugRequired))]
     public string Slug { get; set; }
 }
