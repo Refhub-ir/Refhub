e9ff74c7066622d67c244c0cd4af98ecb75fb499
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-06-11 17:28:40 +0330

    Update validation messages in category view models
    Refactor `CreateCategoryVM` and `UpdateCategoryVM` to use resource-based error messages for validation attributes. Added `using Refhub.Resources` and replaced hardcoded strings with `ErrorMessageResourceType` and `ErrorMessageResourceName` for improved localization support and consistency in error messaging.

--- Refhub/Models/Category/UpdateCategoryVM.cs
+++ Refhub/Models/Category/UpdateCategoryVM.cs
@@ -1,14 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Refhub.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace Refhub.Models.Category;
 
 public class UpdateCategoryVM
 {
     public int Id { get; set; }
-    [Required(ErrorMessage = "لطفا نام دسته بندی را وارد کنید.")]
+    [Required(ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.Book_Title_CategoryRequired))]
     public string Name { get; set; }
-    [Required(ErrorMessage = "لطفا نام اسلاگ را وارد کنید.")]
+    [Required(ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.Name_Slug_Required))]
     public string Slug { get; set; }
-    [Required(ErrorMessage = "لطفا توضیحات دسته بندی را وارد کنید.")]
+    [Required(ErrorMessageResourceType = typeof(Messages),
+       ErrorMessageResourceName = nameof(Messages.Category_Description))]
     public string Description { get; set; }
 }
