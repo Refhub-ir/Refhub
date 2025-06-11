2d43e53b58eca71226eff1c7f4d3f65c63102016
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 21:01:32 +0330

    add model s3Configure

---
+++ Refhub/Models/AppSetting/S3Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace Refhub.Models.AppSetting
+{
+    public class S3Configuration
+    {
+        public string Region { get; set; }
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string BucketName { get; set; }
+        public string ServiceURL { get; set; }
+    }
+}
