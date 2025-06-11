d94d695cc4e65e7ff748d7445673c20748dabb96
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 22:25:07 +0330

    fix: correct method name and parameter in IFileUploaderService interface

--- Refhub/Service/Interface/IFileUploaderService.cs
+++ Refhub/Service/Interface/IFileUploaderService.cs
@@ -2,6 +2,7 @@
 
 public interface IFileUploaderService
 {
-    Task<string> UpdloadFile(IFormFile file, string directoryName, string Type, string Name);
-    Task DeleteFile(string directoryName, string Type, string Name);
+    Task<string> UploadFile(IFormFile file, string directoryName, string Type, string Name);
+  
+    Task DeleteFile(string realUrl);
 }
