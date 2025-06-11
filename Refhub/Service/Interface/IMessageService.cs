da75ac2c2901a38aa61b07897166eb91641fc6a8
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 11:33:45 +0330

    Add IMessageService interface for message retrieval

---
+++ Refhub/Service/Interface/IMessageService.cs
@@ -0,0 +1,8 @@
+﻿namespace Refhub.Service.Interface
+{
+    public interface IMessageService
+    {
+        string Get(string key);
+        string Get(string key, params object[] parameters);
+    }
+}
