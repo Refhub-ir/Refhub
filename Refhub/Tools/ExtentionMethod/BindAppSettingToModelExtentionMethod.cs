6738d8d3f43e78e68c6d4d960ad2e7077ebdd14d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 21:01:46 +0330

    bind appsetting to model s3

---
+++ Refhub/Tools/ExtentionMethod/BindAppSettingToModelExtentionMethod.cs
@@ -0,0 +1,15 @@
+﻿using Refhub.Models.AppSetting;
+using Refhub.Service.Implement;
+using Refhub.Service.Interface;
+
+namespace Refhub.Tools.ExtentionMethod;
+
+public static class BindAppSettingToModelExtentionMethod
+{
+    public static WebApplicationBuilder BindS3Model(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<S3Configuration>(
+            builder.Configuration.GetSection("AWS:S3"));
+        return builder;
+    }
+}
