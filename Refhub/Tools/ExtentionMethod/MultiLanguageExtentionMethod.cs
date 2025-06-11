e8ae42b4a1d7b080a620571e003cdd0991b24091
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 11:33:56 +0330

    Add MultiLanguageExtensionMethod for localization support

---
+++ Refhub/Tools/ExtentionMethod/MultiLanguageExtentionMethod.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Refhub.Service.Implement;
+using Refhub.Service.Interface;
+using Refhub_Ir.Service.Implement;
+using Refhub_Ir.Service.Interface;
+using Refhub_Ir.Service.Interfaces;
+
+namespace Refhub_Ir.Tools.ExtentionMethod;
+
+public static class MultiLanguageExtentionMethod
+{
+    public static WebApplication UseMultiLanguage(this WebApplication app)
+    {
+        var supportedCultures = new List<CultureInfo>()
+        {
+            new CultureInfo("fa-IR"),
+            new CultureInfo("en-US")
+        };
+        var options = new RequestLocalizationOptions()
+        {
+            DefaultRequestCulture = new RequestCulture("fa-IR"),
+            SupportedCultures = supportedCultures,
+            SupportedUICultures = supportedCultures,
+            RequestCultureProviders = new List<IRequestCultureProvider>()
+            {
+                new QueryStringRequestCultureProvider(),
+                new CookieRequestCultureProvider()
+            }
+        };
+        app.UseRequestLocalization(options);
+        return app;
+    }
+
+    public static IMvcBuilder AddMultiLanguage(this IMvcBuilder mvc)
+    {
+       
+        mvc.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, option =>
+        {
+            option.ResourcesPath = "Resources";
+        }).AddDataAnnotationsLocalization();
+        ;
+        return mvc;
+    }
+}
