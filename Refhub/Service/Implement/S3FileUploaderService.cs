7fe2a3a2e88cfa10ea29b95093bd85ce5fcb5806
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-11 22:25:16 +0330

    feat: implement S3FileUploaderService for file upload and deletion using AWS S3

---
+++ Refhub/Service/Implement/S3FileUploaderService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Options;
+using Refhub.Models.AppSetting;
+using Refhub.Service.Interface;
+
+namespace Refhub.Service.Implement
+{
+    namespace S3_Sample.Service
+    {
+        using Amazon;
+        using Amazon.Runtime;
+        using Amazon.S3;
+        using Amazon.S3.Model;
+        using Amazon.S3.Transfer;
+        using Microsoft.Extensions.Configuration;
+
+
+
+
+
+        public class S3FileUploaderService: IFileUploaderService
+        {
+            private readonly string _bucketName;
+            private readonly string _region;
+            private readonly IAmazonS3 _s3Client;
+            private readonly IOptions<S3Configuration> _s3Options;
+
+            public S3FileUploaderService(IOptions<S3Configuration> s3Options)
+            {
+                this._s3Options = s3Options;
+
+                var credentials = new BasicAWSCredentials(s3Options.Value.AccessKey, s3Options.Value.SecretKey);
+
+                var config = new AmazonS3Config
+                {
+                    RegionEndpoint = RegionEndpoint.USEast1, // منطقه ساختگی، چون ArvanRegion اختصاصی داره
+                    ServiceURL = s3Options.Value.ServiceURL,
+                    ForcePathStyle = true // بسیار مهم برای کار با آروان‌کلاد
+                };
+
+                _s3Client = new AmazonS3Client(credentials, config);
+
+                _region = s3Options.Value.Region;
+                _bucketName = s3Options.Value.BucketName;
+            }
+            private string GenerateS3Url(string key)
+            {
+                // برای آروان کلاد:
+                return $"https://{_bucketName}.{_region}/{key}";
+            }
+
+            private string GetKey(string realUrl)
+            {
+                // برای آروان کلاد:
+                
+                return realUrl.Replace($"https://{_bucketName}.{_region}/","");
+            }
+            public async Task<string> UploadFile(IFormFile file, string directoryName, string type, string name)
+            {
+                var bucketName = _bucketName;
+                var key = $"{directoryName}/{type}/{name.Replace(" ","-")}{Path.GetExtension(file.FileName)}";
+
+                using var stream = file.OpenReadStream();
+                var request = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = key,
+                    InputStream = stream,
+                    ContentType = file.ContentType,
+
+                    CannedACL = S3CannedACL.PublicRead
+                };
+
+                await _s3Client.PutObjectAsync(request);
+                return GenerateS3Url(key);
+            }
+
+
+            public async Task DeleteFile(string realUrl)
+            {
+           
+                var key = GetKey(realUrl);
+
+                var request = new DeleteObjectRequest
+                {
+                    BucketName = _bucketName,
+                    Key = key
+                };
+
+                await _s3Client.DeleteObjectAsync(request);
+            }
+        }
+    }
+
+}
