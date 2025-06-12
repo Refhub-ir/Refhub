5aefd5462af60c9991ef74271fbdd3c8fedff7e9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-12 16:48:14 +0330

    Add DuplicateSlugException and EntityNotFoundException classes for improved error handling

---
+++ Refhub/Tools/Exceptions/DuplicateSlugException.cs
@@ -0,0 +1,24 @@
+﻿namespace Refhub.Tools.Exceptions
+{
+    public class DuplicateSlugException: Exception
+    {
+        public DuplicateSlugException(string message): base(message)
+        {
+        }
+        public DuplicateSlugException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+     
+    }
+
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException (string message) : base(message)
+        {
+        }
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
