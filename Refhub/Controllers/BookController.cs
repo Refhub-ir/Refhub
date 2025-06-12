5a7367fc01bcdd595c96e82271612b5cd0a3b8cd
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-06-12 15:23:02 +0330

    Enhance BookController for S3 file downloads
    - Added IFileUploaderService dependency to BookController.
    - Updated DownloadFile method to use file name in route.
    - Implemented error handling for file download failures.
    - Added [Authorize] attribute to DownloadFile method.
    - Removed unused local file path handling code.

--- Refhub/Controllers/BookController.cs
+++ Refhub/Controllers/BookController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Refhub.Service.Implement.S3_Sample.Service;
 using Refhub.Service.Interface;
 
 namespace Refhub.Controllers;
 
-public class BookController(IBookService bookService) : Controller
+public class BookController(IBookService bookService,IFileUploaderService _s3FileUploaderService) : Controller
 {
     [HttpGet("BookDetails/{slug}")]
     public async Task<IActionResult> BookDetails(string slug, CancellationToken ct)
@@ -19,18 +20,23 @@ public class BookController(IBookService bookService) : Controller
 
         return bookDetails == null ? NotFound() : View(bookDetails);
     }
-    [Authorize]
-    [HttpGet]
-    public async Task<IActionResult> DownloadFile(string filePath, CancellationToken ct)
-    {
-        filePath = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot{filePath}");
-        if (string.IsNullOrEmpty(filePath) || !System.IO.File.Exists(filePath))
-        {
-            return NotFound();
-        }
 
-        var fileBytes = await System.IO.File.ReadAllBytesAsync(filePath, ct);
-        return File(fileBytes, "application/octet-stream", Path.GetFileName(filePath));
+
+    [Authorize]
+    [HttpGet("download/{fileName}")]
+    public async Task<IActionResult> DownloadFile(string fileName, CancellationToken ct)
+    {
+        try
+        {
+            var stream = await _s3FileUploaderService.DownloadFileAsync(fileName);
+            var contentType = "application/octet-stream"; // یا محتوای واقعی بر اساس پسوند
+
+            return File(stream, contentType, fileName);
+        }
+        catch (Exception ex)
+        {
+            return NotFound($"خطا در دانلود فایل: {ex.Message}");
+        }
     }
 
     private readonly int _pageSize = 3;
@@ -41,4 +47,6 @@ public class BookController(IBookService bookService) : Controller
 
         return View(viewModel);
     }
+
+
 }
