0751743adf8ad8069038ac6ea5d473d5ee2ec2b2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-12 14:19:54 +0330

    Add filter to Slug index in Books table to support soft delete functionality

---
+++ Refhub/Migrations/20250612104853_AddFilterSlugIndexByIsDeleteOnBook.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Refhub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilterSlugIndexByIsDeleteOnBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_Slug",
+                table: "Books");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Slug",
+                table: "Books",
+                column: "Slug",
+                unique: true,
+                filter: "IsDelete = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_Slug",
+                table: "Books");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Slug",
+                table: "Books",
+                column: "Slug",
+                unique: true);
+        }
+    }
+}
