68e58554ea956fba785895924e776cdfcac91a37
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-12 16:47:39 +0330

    Add MaxLength validation to Name and Slug properties in UpdateCategoryVM

--- Refhub/Models/Category/UpdateCategoryVM.cs
+++ Refhub/Models/Category/UpdateCategoryVM.cs
@@ -8,9 +8,13 @@ public class UpdateCategoryVM
     public int Id { get; set; }
     [Required(ErrorMessageResourceType = typeof(Messages),
         ErrorMessageResourceName = nameof(Messages.Category_NameRequired))]
+    [MaxLength(50, ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.Name_Max_50))]
     public string Name { get; set; }
     [Required(ErrorMessageResourceType = typeof(Messages),
-        ErrorMessageResourceName = nameof(Messages.Name_Slug_Required))]
+        ErrorMessageResourceName = nameof(Messages.Slug_NameRequired))]
+    [MaxLength(450, ErrorMessageResourceType = typeof(Messages),
+        ErrorMessageResourceName = nameof(Messages.Slug_Max_450))]
     public string Slug { get; set; }
     [Required(ErrorMessageResourceType = typeof(Messages),
        ErrorMessageResourceName = nameof(Messages.Category_Description))]
