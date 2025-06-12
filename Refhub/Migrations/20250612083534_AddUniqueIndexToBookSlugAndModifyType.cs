03cd04ef79ea6e35a3569726f24fc209a570ca19
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-12 12:49:50 +0330

    Add migration to create unique index on Book Slug and modify column type

---
+++ Refhub/Migrations/20250612083534_AddUniqueIndexToBookSlugAndModifyType.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Refhub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToBookSlugAndModifyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Books",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Slug",
+                table: "Books",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_Slug",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
