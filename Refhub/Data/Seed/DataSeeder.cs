6f4a07cb3131a5dd3ae9547a9a0bbffd67322ae9
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-06-17 00:05:49 -0800

    add Authors and Books Configuration

--- Refhub/Data/Seed/DataSeeder.cs
+++ Refhub/Data/Seed/DataSeeder.cs
@@ -1,7 +1,34 @@
-﻿namespace Refhub.Data.Seed
-{
-    public class DataSeeder
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Refhub.Data.Context;
 
+namespace Refhub.Data.Seed
+{
+    public static class DataSeeder
+    {
+        public static void SeedInitialData(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            db.Database.Migrate();
+
+            // Authors
+            if (!db.Authors.Any())
+            {
+                var authorPath = Path.Combine(Directory.GetCurrentDirectory(), "SeedData", "AuthorData.xlsx");
+                var authors = ExcelSeeder.ReadAuthorsFromExcel(authorPath);
+                db.Authors.AddRange(authors);
+            }
+
+            // Books
+            if (!db.Books.Any())
+            {
+                var bookPath = Path.Combine(Directory.GetCurrentDirectory(), "SeedData", "BookData.xlsx");
+                var books = ExcelSeeder.ReadBooksFromExcel(bookPath);
+                db.Books.AddRange(books);
+            }
+
+            db.SaveChanges();
+        }
     }
 }
