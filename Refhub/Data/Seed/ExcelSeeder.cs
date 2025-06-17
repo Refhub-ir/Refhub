7716ece9b7b3f7ef5e02ddbe1e763945fd2564e4
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-06-16 23:40:37 -0800

    create ReadBooksFromExcel Method

--- Refhub/Data/Seed/ExcelSeeder.cs
+++ Refhub/Data/Seed/ExcelSeeder.cs
@@ -15,13 +15,13 @@ namespace Refhub.Data.Seed
 
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
-                var worksheet = package.Workbook.Worksheets[0];
-                int rowCount = worksheet.Dimension.Rows;
+                var workSheet = package.Workbook.workSheets[0];
+                int rowCount = workSheet.Dimension.Rows;
 
                 for (int row = 2; row <= rowCount; row++)
                 {
-                    var fullName = worksheet.Cells[row, 1].Text;
-                    var slug = worksheet.Cells[row, 2].Text;
+                    var fullName = workSheet.Cells[row, 1].Text;
+                    var slug = workSheet.Cells[row, 2].Text;
 
                     if (!String.IsNullOrWhiteSpace(fullName) && !String.IsNullOrWhiteSpace(slug))
                         {
@@ -37,5 +37,45 @@ namespace Refhub.Data.Seed
         }
 
         // Read Data from Book File
+        public static List<Book> ReadBooksFromExcel(string filePath)
+        {
+            ExcelPackage.LicenseContext= LicenseContext.NonCommercial;
+            var books = new List<Book>();
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var workSheet = package.Workbook.Worksheets[0];
+                int rowCount = workSheet.Dimension.Rows;
+
+                for(int row = 2; row <= rowCount; row++)
+                {
+                    var title = workSheet.Cells[row, 1].Text;
+                    var slug = workSheet.Cells[row, 2].Text;
+                    var pageCountText = workSheet.Cells[row, 3].Text;
+                    var filePathCell = workSheet.Cells[row, 4].Text;
+                    var imagePath = workSheet.Cells[row, 5].Text;
+                    var categoryIdText = workSheet.Cells[row, 6].Text;
+                    var userId = workSheet.Cells[row, 7].Text;
+
+
+                    if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(slug))
+                        continue;
+
+                    int.TryParse(pageCountText, out int pageCount);
+                    int.TryParse(categoryIdText, out int categoryId);
+
+                    books.Add(new Book
+                    {
+                        Title = title,
+                        Slug = slug,
+                        PageCount = pageCount,
+                        FilePath = filePathCell,
+                        ImagePath = imagePath,
+                        CategoryId = categoryId,
+                        UserId = string.IsNullOrWhiteSpace(userId) ? null : userId
+                    });
+                }
+            }
+            return books;
+        }
     }
 }
