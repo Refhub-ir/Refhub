d8d616c4c8675bd419db54449301b0a540c14c5c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-01 17:08:09 +0330

    Fix regular expression for password validation in RegisterVM.cs

--- Refhub/Models/Users/RegisterVM.cs
+++ Refhub/Models/Users/RegisterVM.cs
@@ -17,8 +17,8 @@ public class RegisterVM
        ErrorMessageResourceType = typeof(Messages),
        ErrorMessageResourceName = nameof(Messages.Password_Required))]
     [RegularExpression(
-// At least one letter, one digit, 6-64 chars, allow specials.
-        @"^(?=.*[A-Za-z])(?=.*\d).{6,64}$",
+        // At least one letter, one digit, 6-64 chars, allow specials.
+        @"^(?=.*[A-Za-z])(?=.*\d)[^\s]{6,64}$",
        ErrorMessageResourceType = typeof(Messages),
        ErrorMessageResourceName = nameof(Messages.Password_Regex_Invalid))]
     [DataType(DataType.Password)]
