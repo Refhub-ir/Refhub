7e7822e9a53887cb64ed4f448790ac4389af944b
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-13 00:05:14 +0330

    add BucketNameStatic

---
+++ Refhub/Tools/Static/BucketNameStatic.cs
@@ -0,0 +1,18 @@
+﻿using Refhub.Models.Enums;
+
+namespace Refhub.Tools.Static;
+
+public static class BucketNameStatic
+{
+    public static string GetName(BucketNames bucketNames)
+    {
+        return bucketNames switch
+        {
+            BucketNames.BookPdf => nameof(BucketNames.BookPdf),
+            BucketNames.BookImages => nameof(BucketNames.BookImages),
+
+
+            _ => throw new ArgumentOutOfRangeException(nameof(bucketNames), $"Unsupported directory type: {bucketNames}")
+        };
+    }
+}
